ca9e5d9ff7872f13b876a120674baa3a4edb2a05
maximastashkin <maksim.astash@gmail.com>
2022-02-18 10:59:29 +0300

    Initial commit

---
+++ src/main/java/ru/rsreu/contests_system/RsreuContestsSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.rsreu.contests_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class    RsreuContestsSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsreuContestsSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rsreu/contests_system/RsreuContestsSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsreu.contests_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsreuContestsSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
